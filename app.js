8ebc0f89032e8dda3ebb8a069bec4dc51fee725e
lea.arturi <lea.arturi@gmail.com>
2021-02-14 16:30:23 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,16 @@
+require('colors');
+
+const { inquirerMenu, pausa } = require('./helpers/inquirer');
+
+const main = async () => {
+
+    let opt = '';
+
+    do {
+       opt = await inquirerMenu();
+       await pausa();
+    } while (opt !== '0');
+    
+};
+
+main();
