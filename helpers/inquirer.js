f7e5fdf4544e02308ad258784dc4bd9cd0ffaeb3
lea.arturi <lea.arturi@gmail.com>
2021-02-14 17:17:23 -0300

    Avances

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -70,7 +70,30 @@ const pausa = async () => {
 
 };
 
+const leerInput = async ( message ) => {
+
+    const pregunta = [
+        {
+            type: 'input',
+            name: 'desc',
+            message,
+            validate(value) {
+                if (value.length === 0) {
+                    return 'Por favor ingrese un valor';
+                } else {
+                    return true;
+                }
+            }
+        }
+    ];
+
+    const { desc } = await inquirer.prompt(pregunta);
+    return desc;
+
+};
+
 module.exports = {
     inquirerMenu,
-    pausa
+    pausa,
+    leerInput
 };
